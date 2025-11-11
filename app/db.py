992335f05056c7aa7e39bd072e6bd756bb0119ad
luxurahair <info@luxuradistribution.com>
2025-11-11 15:03:38 -0500

    db.py

--- app/db.py
+++ app/db.py
@@ -1,10 +1,5 @@
 import os
 from sqlmodel import create_engine
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///app.db"  # fallback local
-)
-
-# echo=False pour des logs propres
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
 engine = create_engine(DATABASE_URL, echo=False)
