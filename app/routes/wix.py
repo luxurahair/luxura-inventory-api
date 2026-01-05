4e8cffc020e390938b31e58530da4fe90bd239dc
luxurahair <info@luxuradistribution.com>
2026-01-05 16:18:32 -0500

    Import normalize_product from catalog_normalizer

--- app/routes/wix.py
+++ app/routes/wix.py
@@ -13,6 +13,7 @@ from sqlmodel import Session, select
 
 from app.db.session import get_session
 from app.models.product import Product
+from app.services.catalog_normalizer import normalize_product
 
 
 router = APIRouter(prefix="/wix", tags=["wix"])
