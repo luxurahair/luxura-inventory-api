e7afbbf45b18e62835dda572aa9b15668ff7d4f5
luxurahair <info@luxuradistribution.com>
2026-01-14 10:43:45 -0500

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,4 +7,5 @@ __all__ = [
     "Product", "ProductCreate", "ProductRead", "ProductUpdate",
     "Salon", "SalonCreate", "SalonRead", "SalonUpdate",
     "InventoryItem", "InventoryRead",
+    "SyncRun",
 ]
